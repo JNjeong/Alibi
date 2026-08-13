3904f4f3c462dbc6659c3f51dfbfe7892915cbb1
JNjeong <wnsghd0902@gmail.com>
2026-08-14 01:14:12 +0900

    upload final_version

--- frontend/src/api/gameLog_api.js
+++
@@ -1,6 +0,0 @@
-import api from "./axios"
-
-export const getGameLogByRoomId = async (roomId) => {
-  const response = await api.get(`/game-logs/room/${roomId}`)
-  return response.data
-}
--- frontend/src/api/index.js
+++
@@ -1,2 +0,0 @@
-export * from "./game_api"
-export * from "./auth_api"
